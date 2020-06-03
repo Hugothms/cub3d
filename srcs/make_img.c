3c5bbc4811bad38c090c54255048dfa65edd43a2
Hugothms <hugothms@gmail.com>
2020-06-03 20:55:27 +0200

    4 side

--- srcs/make_img.c
+++ srcs/make_img.c
@@ -6,7 +6,7 @@
 /*   By: hugothms <hugothms@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/15 20:08:47 by hugothms          #+#    #+#             */
-/*   Updated: 2020/06/03 16:17:54 by hugothms         ###   ########.fr       */
+/*   Updated: 2020/06/03 20:49:37 by hugothms         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -140,23 +140,18 @@ void	make_img(t_img *img, t_scene *scene)
 		double cameraX = 2 * x / (double)scene->resolution.w - 1; //x-coordinate in camera space
 		double rayDirX = scene->dir.x + scene->plane.x * cameraX;
 		double rayDirY = scene->dir.y + scene->plane.y * cameraX;
-
 		int mapX = (int)scene->pos.x;
 		int mapY = (int)scene->pos.y;
-
 		//length of ray from current position to next x or y-side
 		double sideDistX;
 		double sideDistY;
-
 		//length of ray from one x or y-side to next x or y-side
 		double deltaDistX = fabs(1 / rayDirX);
 		double deltaDistY = fabs(1 / rayDirY);
 		double perpWallDist;
-
 		//what direction to step in x or y-direction (either +1 or -1)
 		int stepX;
 		int stepY;
-
 		int hit = 0; //was there a wall hit?
 		int side; //was a NS or a EW wall hit?
 		//calculate step and initial sideDist
@@ -188,13 +183,19 @@ void	make_img(t_img *img, t_scene *scene)
 			{
 				sideDistX += deltaDistX;
 				mapX += stepX;
-				side = 0;
+				if (rayDirX > 0)
+					side = 0;
+				else
+					side = 2;
 			}
 			else
 			{
 				sideDistY += deltaDistY;
 				mapY += stepY;
-				side = 1;
+				if (rayDirY > 0)
+					side = 1;
+				else
+					side = 3;
 			}
 			//Check if ray has hit a wall
 			// printf("map%d:%d\n", mapX, mapY);
@@ -207,7 +208,7 @@ void	make_img(t_img *img, t_scene *scene)
 		// printf("rayDirX: %f\trayDirY:%f\n", rayDirX, rayDirY);
 		// printf("scene->pos.x: %f\tscene->pos.y:%f\n", scene->pos.x, scene->pos.y);
 		// printf("side:%d\n", side);
-		if(side == 0)
+		if(side % 2 == 0)
 			perpWallDist = (mapX - scene->pos.x + (1 - stepX) / 2.) / rayDirX;
 		else
 			perpWallDist = (mapY - scene->pos.y + (1 - stepY) / 2.) / rayDirY;
@@ -216,7 +217,6 @@ void	make_img(t_img *img, t_scene *scene)
 		//Calculate height of line to draw on screen
 		int lineHeight = (scene->resolution.h / perpWallDist);
 		// printf("lineHeight: %d\n", lineHeight);
-
 		//calculate lowest and highest pixel to fill in current stripe
 		int drawStart = -lineHeight / 2 + scene->resolution.h / 2;
 		if(drawStart < 0)
@@ -224,13 +224,14 @@ void	make_img(t_img *img, t_scene *scene)
 		int drawEnd = lineHeight / 2 + scene->resolution.h / 2;
 		if(drawEnd >= scene->resolution.h)
 			drawEnd = scene->resolution.h - 1;
-
 		//choose wall color
 		t_rgb *color;
 		switch(side)
 		{
 			case 0:color = int_to_rgb(0,255,255);break;
 			case 1:color = int_to_rgb(255,0,255);break;
+			case 2:color = int_to_rgb(255,0,0);break;
+			case 3:color = int_to_rgb(0,255,0);break;
 			default: color = int_to_rgb(0,255,0); break;
 		}
 		// //give x and y sides different brightness
