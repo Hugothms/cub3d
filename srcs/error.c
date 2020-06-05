79e87252df1edc0a026948895a988f3ab14d9d2e
Hugothms <hugothms@gmail.com>
2020-06-05 08:54:42 +0200

    error message

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: hugothms <hugothms@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/10 18:54:35 by hthomas           #+#    #+#             */
-/*   Updated: 2020/05/04 11:39:28 by hugothms         ###   ########.fr       */
+/*   Updated: 2020/06/05 08:53:47 by hugothms         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	print_err_and_exit(char *str, int err)
 {
-	ft_putstr_fd("Error: ", STDERR_FILENO);
+	ft_putstr_fd("Error\n", STDERR_FILENO);
 	ft_putstr_fd(str, STDERR_FILENO);
 	ft_putstr_fd(" (exit code: ", STDERR_FILENO);
 	ft_putnbr_fd(err, STDERR_FILENO);
