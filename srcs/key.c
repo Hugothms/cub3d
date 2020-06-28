259254d901dc2d19e43a36d05c3ae5df3c25e1d2
Hugothms <hugothms@gmail.com>
2020-06-28 13:21:33 +0200

    key turn

--- srcs/key.c
+++ srcs/key.c
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/20 16:49:23 by hthomas           #+#    #+#             */
-/*   Updated: 2020/06/24 12:51:39 by hthomas          ###   ########.fr       */
+/*   Updated: 2020/06/28 13:20:25 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,14 @@ int		key_release(int keycode, t_move *move)
 		move->up = 0;
 	else if (keycode == ARROW_DOWN || keycode == KEY_S)
 		move->down = 0;
-	else if (keycode == (AZERTY ? KEY_A : KEY_Q))
-		move->left = 0;
-	else if (keycode == KEY_E)
-		move->right = 0;
-	else if (keycode == ARROW_LEFT || keycode == (AZERTY ? KEY_Q : KEY_A))
+	else if (keycode == ARROW_LEFT || keycode == (AZERTY ? KEY_A : KEY_Q))
 		move->turn_left = 0;
-	else if (keycode == ARROW_RIGHT || keycode == KEY_D)
+	else if (keycode == ARROW_RIGHT || keycode == KEY_E)
 		move->turn_right = 0;
+	else if (keycode == (AZERTY ? KEY_Q : KEY_A))
+		move->left = 0;
+	else if (keycode == KEY_D)
+		move->right = 0;
 	return (0);
 }
 
@@ -44,14 +44,14 @@ int		key_push(int keycode, t_move *move)
 		move->up = 1;
 	else if (keycode == ARROW_DOWN || keycode == KEY_S)
 		move->down = 1;
-	else if (keycode == (AZERTY ? KEY_A : KEY_Q))
-		move->left = 1;
-	else if (keycode == KEY_E)
-		move->right = 1;
-	else if (keycode == ARROW_LEFT || keycode == (AZERTY ? KEY_Q : KEY_A))
+	else if (keycode == ARROW_LEFT || keycode == (AZERTY ? KEY_A : KEY_Q))
 		move->turn_left = 1;
-	else if (keycode == ARROW_RIGHT || keycode == KEY_D)
+	else if (keycode == ARROW_RIGHT || keycode == KEY_E)
 		move->turn_right = 1;
+	else if (keycode == (AZERTY ? KEY_Q : KEY_A))
+		move->left = 1;
+	else if (keycode == KEY_D)
+		move->right = 1;
 	return (0);
 }
 
